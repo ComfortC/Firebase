1031f843ffb0507dde60e75c8f03eff8f9d56aa1
ComfortC <comfortchino@gmail.com>
2016-08-06 02:27:50 +0200

    Writting to the Database

--- app/src/main/java/com/example/khumalo/firebase/MainActivity.java
+++ app/src/main/java/com/example/khumalo/firebase/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.khumalo.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Firebase myFirebaseRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        myFirebaseRef = new Firebase("https://handy-sensor-136618.firebaseio.com/");
+    }
+
+    public void WriteToDatabase(View view) {
+        myFirebaseRef.child("Comfort").setValue("My First Ever BackEndMessage");
     }
 }
