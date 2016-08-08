20f599208fbfe5c20490fe412f9eb923b2b151a2
ComfortC <comfortchino@gmail.com>
2016-08-08 16:53:35 +0200

    Starter Code For Shopping ListPlusPlus

--- app/src/main/java/com/example/khumalo/firebase/MainActivity.java
+++ app/src/main/java/com/example/khumalo/firebase/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.khumalo.firebase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
+import com.example.khumalo.firebase.activeLists.AddListDialogFragment;
+import com.example.khumalo.firebase.activeLists.ShoppingListsFragment;
+import com.example.khumalo.firebase.meals.AddMealDialogFragment;
+import com.example.khumalo.firebase.meals.MealsFragment;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -12,30 +24,144 @@ import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    Firebase myFirebaseRef;
-    TextView Message;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Message = (TextView)findViewById(R.id.message_display_textView);
-        Firebase.setAndroidContext(this);
-        myFirebaseRef = new Firebase("https://handy-sensor-136618.firebaseio.com/");
-        myFirebaseRef.child("Comfort").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                Message.setText(value);
-            }
 
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-               Message.setText("An Error Occured");
-            }
-        });
+        /**
+         * Link layout elements from XML and setup the toolbar
+         */
+        initializeScreen();
     }
 
-    public void WriteToDatabase(View view) {
-        myFirebaseRef.child("Comfort").setValue("My First Ever BackEndMessage");
+
+    /**
+     * Override onOptionsItemSelected to use main_menu instead of BaseActivity menu
+     *
+     * @param menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Inflate the menu; this adds items to the action bar if it is present. */
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Override onOptionsItemSelected to add action_settings only to the MainActivity
+     *
+     * @param item
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * Link layout elements from XML and setup the toolbar
+     */
+    public void initializeScreen() {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        /**
+         * Create SectionPagerAdapter, set it as adapter to viewPager with setOffscreenPageLimit(2)
+         **/
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(adapter);
+        /**
+         * Setup the mTabLayout with view pager
+         */
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /**
+     * Create an instance of the AddList dialog fragment and show it
+     */
+    public void showAddListDialog(View view) {
+        /* Create an instance of the dialog fragment and show it */
+        DialogFragment dialog = AddListDialogFragment.newInstance();
+        dialog.show(MainActivity.this.getFragmentManager(), "AddListDialogFragment");
+    }
+
+    /**
+     * Create an instance of the AddMeal dialog fragment and show it
+     */
+    public void showAddMealDialog(View view) {
+        /* Create an instance of the dialog fragment and show it */
+        DialogFragment dialog = AddMealDialogFragment.newInstance();
+        dialog.show(MainActivity.this.getFragmentManager(), "AddMealDialogFragment");
+    }
+
+    /**
+     * SectionPagerAdapter class that extends FragmentStatePagerAdapter to save fragments state
+     */
+    public class SectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Use positions (0 and 1) to find and instantiate fragments with newInstance()
+         *
+         * @param position
+         */
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment fragment = null;
+
+            /**
+             * Set fragment to different fragments depending on position in ViewPager
+             */
+            switch (position) {
+                case 0:
+                    fragment = ShoppingListsFragment.newInstance();
+                    break;
+                case 1:
+                    fragment = MealsFragment.newInstance();
+                    break;
+                default:
+                    fragment = ShoppingListsFragment.newInstance();
+                    break;
+            }
+
+            return fragment;
+        }
+
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        /**
+         * Set string resources as titles for each fragment by it's position
+         *
+         * @param position
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.pager_title_shopping_lists);
+                case 1:
+                default:
+                    return getString(R.string.pager_title_meals);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/khumalo/firebase/ShoppingListApplication.java
@@ -0,0 +1,13 @@
+ package com.example.khumalo.firebase;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
